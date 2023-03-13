c9a2b051f0c3fa71b409fac142d4f8a9de62576c
gabrielmlaker <mlakergabriel@gmail.com>
2023-03-13 19:48:30 -0300

    Department entity class

---
+++ src/main/java/com/jdbcproject/Program.java
@@ -0,0 +1,13 @@
+package com.jdbcproject;
+
+import com.jdbcproject.model.entities.Department;
+
+public class Program {
+    public static void main(String[] args) {
+        
+       Department obj = new Department(1,"Books");
+       System.out.println(obj);
+
+
+    }
+}
