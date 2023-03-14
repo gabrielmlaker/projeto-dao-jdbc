5884cfbdac1a3d289ea11dc25ff33152fe5a83af
gabrielmlaker <mlakergabriel@gmail.com>
2023-03-13 23:04:18 -0300

    findByDepartment Implementation

--- src/main/java/com/jdbcproject/Program.java
+++ src/main/java/com/jdbcproject/Program.java
@@ -1,17 +1,27 @@
 package com.jdbcproject;
 
+import java.util.List;
+
 import com.jdbcproject.model.dao.DaoFactory;
 import com.jdbcproject.model.dao.SellerDao;
+import com.jdbcproject.model.entities.Department;
 import com.jdbcproject.model.entities.Seller;
 
 public class Program {
     public static void main(String[] args) {
 
-        System.out.println("=== TEST 1: seller findById ===");
         SellerDao sellerDao = DaoFactory.createSellerDao();
-        Seller seller = sellerDao.findById(3);
 
+        System.out.println("=== TEST 1: seller findById ===");
+        Seller seller = sellerDao.findById(3);
         System.out.println(seller);
 
+        System.out.println("=== TEST 2: seller findByDepartment ===");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for (Seller obj : list) {
+            System.out.println(obj);
+            
+        }
     }
 }
--- src/main/java/com/jdbcproject/model/dao/SellerDao.java
+++ src/main/java/com/jdbcproject/model/dao/SellerDao.java
@@ -2,12 +2,14 @@ package com.jdbcproject.model.dao;
 
 import java.util.List;
 
+import com.jdbcproject.model.entities.Department;
 import com.jdbcproject.model.entities.Seller;
 
 public interface SellerDao {
     void insert(Seller obj);
-    void update(Seller obj);
-    void deleteById(Integer id);
-    Seller findById(Integer id);
-    List<Seller> findAll();
+	void update(Seller obj);
+	void deleteById(Integer id);
+	Seller findById(Integer id);
+	List<Seller> findAll();
+	List<Seller> findByDepartment(Department department);
 }
